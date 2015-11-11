5d054f2f973face7079679b024263e7c7a304c0d
Anthony Romaniello <aromaniello70@gmail.com>
2015-11-11 18:33:37 -0500

    Full Commented Code
    It’s done. The parse function can be really confusing so there are
    literally paragraph comments explaining it. This should be more
    commenting than you could ever need, so there ya go.

--- basic.py
+++ basic.py
@@ -7,8 +7,8 @@ def open_file(filename):
 	data += "<EOF>"
 	return data
 
-# Lex function - interprets the source file and identifies
-# things. For example it will recognize 'preach'.
+# Lex function - interprets the source file
+# This returns a list of recognized syntax
 def lex(filecontents):
 
 	# Defining variables
@@ -22,7 +22,7 @@ def lex(filecontents):
 	# Splits contents of the file into chars in a list
 	filecontents = list(filecontents)
 
-	# Runs for each character in the list
+	# For loop runs for each character in the list
 	for char in filecontents:
 		tok += char
 		if tok == " ":
@@ -75,19 +75,34 @@ def lex(filecontents):
 	print(tokens) # Prints full list of recognized syntax
 	return tokens # Returns full list of recognized syntax
 		
-# Parse function - puts it all together
+# Parse function - takes the list of syntax
+# from  lex and applies the defined action.
 def parse(toks):
-# Parse takes the completed tokens list as input.
-# Using this list of recognized syntax, it parses
-# the code, deciding what to do with each term.
 	i = 0
+	# While loop runs for each term in the list
 	while(i < len(toks)):
-		# Runs for each recognized term in the list
+
+		# The following confusing if statement will run if it
+			# recognizes any of the terms we defined for it
+			# earlier, via the toks list. Then it will print
+			# the correct part of the list item based on what
+			# type it is, which it knows from the first part
+			# of each list item.
 		if toks[i] + " " + toks[i+1][0:6] == "preach STRING" or toks[i] + " " + toks[i+1][0:3] == "preach NUM" or toks[i] + " " + toks[i+1][0:4] == "preach EXPR":
+			# Pretty confusing, this took me a while to get the list stuff right
+			# The reason for starting the print at various locations is that
+			# the first part of each item is the datatype (string, expr, num, etc)
+			# followed by a colon. So you have to start the print after the colon.
+
+			# If it is a string, print the string
 			if toks[i+1][0:6] == "STRING":
 				print(toks[i+1][7:])
+
+			# If it is a number, print the number
 			elif toks[i+1][0:3] == "NUM":
 				print(toks[i+1][4:])
+
+			# If it is an expression, print the expression
 			elif toks[i+1][0:4] == "EXPR":
 				print(toks[i+1][5:])
 			i += 2
